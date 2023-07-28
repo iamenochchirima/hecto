28ffdee1c8c40dae7f9cc6ac6ad98fd08288d38e
iamenochchirima <iamenochchirima@gmail.com>
2023-07-28 09:02:57 +0200

    Add: raw key presses observing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,29 @@
-use std::io::{self, Read};
+use std::io::{self, stdout, Read};
+use termion::raw::IntoRawMode;
 
 fn main() {
-    for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c)
+    let _stdout = stdout().into_raw_mode().unwrap();
+
+    for result in io::stdin().bytes() {
+        match result {
+            Ok(b) => {
+                let c = b as char;
+                println!("{}", c);
+
+                if c.is_control() {
+                    println!("{:?}  \r ", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+
+                if c == 'q' {
+                    break;
+                }
+            }
+            Err(err) => {
+                eprintln!("Error reading input: {:?}", err);
+                break;
+            }
+        }
     }
 }
