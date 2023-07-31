eaf14a16a23d8f7da00326b208e29bb43cd7e3ec
iamenochchirima <iamenochchirima@gmail.com>
2023-07-31 10:00:00 +0200

    Added editor module

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,7 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+mod editor;
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for result in io::stdin().bytes() {
-        match result {
-            Ok(b) => {
-                let c = b as char;
-                println!("{}", c);
-
-                if c.is_control() {
-                    println!("{:?}  \r ", b);
-                } else {
-                    println!("{:?} ({})\r", b, c);
-                }
-
-                if c == 'q' {
-                    break;
-                }
-            }
-            Err(err) => {
-                eprintln!("Error reading input: {:?}", err);
-                break;
-            }
-        }
-    }
+    let editor = Editor::default();
+    editor.run();
 }
